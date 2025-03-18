d0f7f714d9886756cddecf6244e6daf387511e74
Raja Singh <raja.kumar@antrazal.com>
2025-03-18 19:16:31 +0530

    Linkedlist

---
+++ Day11/LinkedListExample.java
@@ -0,0 +1,11 @@
+package Day11;
+import java.util.*;
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.add(100);
+        l.add(1,12);
+    
+        System.out.println("" + l);
+    }
+}
